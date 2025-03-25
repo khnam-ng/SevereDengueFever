a2bd9b09355472c6b0537bc9d30104c2fb03d190
smivys <khanhnamxz@hotmail.com>
2025-03-25 16:53:45 +0100

    import csv file

---
+++ src/prefix_path.py
@@ -0,0 +1,2 @@
+full_data_path = "/home/smivys/Sorbonne/108/m2_internship/DHF/data/Data_2017_2019_(1).csv"
+redundant_cols = ['ID BN ','STT','Full name','Ward','District','Hospitalization day','Discharge day','Patient No'] 

29791df73d54a83ee35a5657a574259f0f3090de
smivys <khanhnamxz@hotmail.com>
2025-03-31 07:56:25 +0200

    1st try on model, drop strat2

--- src/prefix_path.py
+++ src/prefix_path.py
@@ -1,6 +1,7 @@
 full_data_path = "/home/smivys/Sorbonne/108/m2_internship/DHF/data/Data_2017_2019_(1).csv"
 redundant_cols = ['ID BN ','STT','Full name','Blood group','Ward','District','Hospitalization day','Discharge day','Patient No',\
-                  'Storage No','Characteristic fever '] 
+                  'Storage No','Temperature','Characteristic fever ']
+redundant_rows = [143, 144, 145, 225, 226, 227, 262] 
 additional_diseases = ['Diabetes','HBP','Hepatitis','Gastritis','pregnancy','Other']
 missing_2017 = ['Level of temp','Duration fever'] 
 hematoma_cols = ['Tourniquet test','Petechiae','Purpura','Ecchymoses','Muscle bleeding']

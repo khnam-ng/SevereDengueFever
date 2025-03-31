1254da3cf0a06ef40c3068cdb0f72f055e139b7d
smivys <khanhnamxz@hotmail.com>
2025-04-01 00:05:53 +0200

    ready for LSTM part

--- src/prefix_path.py
+++ src/prefix_path.py
@@ -1,5 +1,5 @@
 full_data_path = "/home/smivys/Sorbonne/108/m2_internship/DHF/data/Data_2017_2019_(1).csv"
-redundant_cols = ['ID BN ','STT','Full name','Blood group','Ward','District','Hospitalization day','Discharge day','Patient No',\
+redundant_cols = ['ID BN ','STT','Full name','Age','Blood group','Ward','District','Hospitalization day','Discharge day','Patient No',\
                   'Storage No','Temperature','Characteristic fever ']
 redundant_rows = [143, 144, 145, 225, 226, 227, 262] 
 additional_diseases = ['Diabetes','HBP','Hepatitis','Gastritis','pregnancy','Other']
@@ -7,3 +7,5 @@ missing_2017 = ['Level of temp','Duration fever']
 hematoma_cols = ['Tourniquet test','Petechiae','Purpura','Ecchymoses','Muscle bleeding']
 bleeding_cols = ['Gum','Nose','Gastrointestine','Urology','Vaginal']
 fillna_values = {'pregnancy': 0, 'Tourniquet test': 0}
+
+heart_index = ['Pulse rate','Systolic BP','Diastolic BP']

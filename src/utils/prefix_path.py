d02fa83a795f997a934734f644489f000b202347
smivys <khanhnamxz@hotmail.com>
2025-04-16 16:08:53 +0200

    3rd_try

--- src/utils/prefix_path.py
+++ src/utils/prefix_path.py
@@ -1,7 +1,7 @@
 FULL_DATA_PATH = "/home/smivys/Sorbonne/108/m2_internship/DHF/data/Data_2017_2019_(1).csv"
 REDUNDANT_COLS = ['ID BN ','STT','Full name','Age','Blood group','Ward','District','Hospitalization day','Discharge day','Patient No',\
                   'Storage No','Temperature','Characteristic fever ']
-REDUNDANT_ROWS = [143, 144, 145, 225, 226, 227, 262] 
+REDUNDANT_ROWS = [143, 144, 145, 225, 226, 227, 250, 262] 
 ADDITIONAL_DISEASES = ['Diabetes','HBP','Hepatitis','Gastritis','pregnancy','Other']
 MISSING_2017 = ['Level of temp','Duration fever'] 
 HEMATOMA_COLS = ['Tourniquet test','Petechiae','Purpura','Ecchymoses','Muscle bleeding']
